ae72424bcc5fc9de1aedf75be88883e5d7f388e2
Mark123M <mark123ymm@gmail.com>
2022-12-29 01:59:22 -0500

    finalizing plugin features

--- ChatReminder.plugin.js
+++ ChatReminder.plugin.js
@@ -63,7 +63,7 @@ const config = {
           title: "Deployment",
           type: "fixed",
           items: [
-          "Auto-Reminder System: Looks through dms and stores ongoing conversations. For every interval of time (set by user), a modal will pop up to display the last messages that you haven't replied to.",      
+          "Auto-Reminder System: For every interval of time (set by user), a modal will pop up to display the messages in your DM convos that you haven't or forgot to reply to",
           "Manual-Reminder System: Right click a message and click \"Remind Me!\" to set a reminder that displays a modal for the message after a set amount of time."
           ]
         }
